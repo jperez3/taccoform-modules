c4221590fefb61ed8618372f6e9275b1139024a4
jperez3 <joseph.perez.la@gmail.com>
2021-04-07 20:39:08 -0700

    adding droplet module for taccoform blog

---
+++ vendors/digitalocean/droplet/variables.tf
@@ -0,0 +1,13 @@
+variable "env" {
+  description = "a short and unique environment name"
+  default     = "prod"
+}
+
+variable "region" {
+  description = "a DigitalOcean provided locale"
+  default     = "sfo2"
+}
+
+variable "service" {
+  description = "a short and unique service name"
+}

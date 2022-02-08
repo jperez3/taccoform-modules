86c2db2778c723220154b089a25b9429598ac2ca
jperez3 <joe@ordisius.com>
2022-02-07 20:03:19 -0500

    updating variable

--- tacco-corp/aws/global/base/dns.tf
+++ tacco-corp/aws/global/base/dns.tf
@@ -2,6 +2,6 @@ module "dns_zone" {
   source = "git::git@github.com:jperez3/taccoform-modules.git//vendors/aws/dns/zone?ref=region-base"
 
   domain = "tacoform.com"
-  env    = "prod"
+  env    = var.env
 
 }

02ec10a0e3295d715b276800a81a2a5ab44e1e43
jperez3 <joe@ordisius.com>
2022-02-21 09:20:16 -0600

    fixing typo

--- vendors/github/repo/repo.tf
+++ vendors/github/repo/repo.tf
@@ -2,7 +2,7 @@ resource "github_repository" "service" {
   name        = var.service
   description = "github repo for ${var.service}"
 
-  visibility = var.repo_visbility
+  visibility = var.repo_visibility
 
   template {
     owner      = var.template_owner

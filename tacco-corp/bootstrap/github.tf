6bc9c687654f8e3cb95bc8eaa5372a15dad98e67
jperez3 <joe@ordisius.com>
2022-02-21 10:01:22 -0600

    adding github enable switch

--- tacco-corp/bootstrap/github.tf
+++ tacco-corp/bootstrap/github.tf
@@ -1,5 +1,7 @@
 module "repo" {
   source = "git::git@github.com:jperez3/taccoform-modules.git//vendors/github/repo?ref=bootstrap"
 
+  count = var.enable_github_repo
+
   service = var.service
 }
--- tacco-corp/bootstrap/variables.tf
+++ tacco-corp/bootstrap/variables.tf
@@ -1,3 +1,8 @@
 variable "service" {
-    description = "unique service name"
+  description = "unique service name"
+}
+
+variable "enable_github_repo" {
+  description = "enables/disables creation of github repo"
+  default     = 1
 }

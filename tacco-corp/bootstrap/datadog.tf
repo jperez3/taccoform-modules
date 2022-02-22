61777848ecb5dc19a2e8b5f6a9f4135b85f79d14
jperez3 <joe@ordisius.com>
2022-02-22 09:38:26 -0600

    adding new providers

---
+++ tacco-corp/bootstrap/datadog.tf
@@ -0,0 +1,23 @@
+resource "datadog_monitor" "service" {
+  name               = "basic monitor for ${var.service}"
+  type               = "metric alert"
+  message            = "Monitor triggered. Notify: @operations-team"
+  escalation_message = "Escalation message @pagerduty"
+
+  query = "avg(last_1h):avg:aws.ec2.cpu{environment:prod,host:foo} by {host} > 4"
+
+  monitor_thresholds {
+    warning           = 2
+    warning_recovery  = 1
+    critical          = 4
+    critical_recovery = 3
+  }
+
+  notify_no_data    = false
+  renotify_interval = 60
+
+  notify_audit = false
+  include_tags = true
+
+  tags = ["foo:bar", "baz"]
+}
--- tacco-corp/bootstrap/provider.tf
+++ tacco-corp/bootstrap/provider.tf
@@ -1,5 +1,13 @@
 terraform {
   required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 4.2.0"
+    }
+    datadog = {
+      source = "DataDog/datadog"
+      version = "~> 3.8.1"
+    }
     github = {
       source  = "integrations/github"
       version = "~> 4.20.0"
